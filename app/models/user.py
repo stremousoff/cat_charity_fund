93cc29e84eb4bea0415e76d92760ba4f3668a8aa
Anton Stremousov <stremousoff@gmail.com>
2024-11-15 00:17:46 +0300

    add_description_for_model

--- app/models/user.py
+++ app/models/user.py
@@ -5,3 +5,10 @@ from app.core.db import Base
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     pass
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"id={self.id}, "
+            f"email={self.email}, "
+        )
